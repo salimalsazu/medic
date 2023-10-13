3bb843cda0f440708f6f4e90eb44d45bc3b33f2d
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-10-14 00:22:52 +0600

    [Done] Appointment

---
+++ src/app/modules/appointmentBooking/appointmentBooking.constant.ts
@@ -0,0 +1,19 @@
+export const appointmentFilterableFields: string[] = [
+    'slotId',
+    'searchTerm',
+    'appointmentDate',
+    'serviceId'
+  ];
+  
+  export const appointmentSearchableFields: string[] = [
+      'serviceId',
+      'appointmentDate',
+      'slotId'    
+  ];
+  
+
+  export const appointmentFields: string[] = ['profileId'];
+  export const appointmentRelationalFieldsMapper: { [key: string]: string } = {
+    profileId: 'profileId',
+  };
+  
